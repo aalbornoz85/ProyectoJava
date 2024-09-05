5df6ddfbd81456fa83a273966d1688eba73f54c4
aalbornoz85 <a.albornoz@outlook.com>
2024-09-05 16:04:21 -0400

    actualiza1

---
+++ src/ejercicios/Ejercicio_02.java
@@ -0,0 +1,16 @@
+package ejercicios;
+
+public class Ejercicio_02 {
+    public static void main(String[] args) {
+        // Declarar las variables
+        int a = 5;
+        int b = 10;
+
+        // Sumar las variables
+        int suma = a + b;
+
+        // Mostrar el resultado
+        System.out.println("La suma de " + a + " y " + b + " es " + suma);
+    }
+
+}
