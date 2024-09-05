bbdcd09a5b24a671f7666ab94f2d8066938b1745
aalbornoz85 <a.albornoz@outlook.com>
2024-09-05 17:10:59 -0400

    Actualizo ejercicio 3

--- src/ejercicios/Ejercicio_03.java
+++ src/ejercicios/Ejercicio_03.java
@@ -8,6 +8,7 @@ public class Ejercicio_03 {
         // Verificar si el número es par o impar
         if (numero % 2 == 0) {
             System.out.println("El número " + numero + " es par.");
+
         } else {
             System.out.println("El número " + numero + " es impar.");
         }
